af78da05fd932e7f7f545bd0296f4fe7ff1bb4f7
ilhaambintangg <ilhambintangg123@gmail.com>
2026-09-09 20:44:18 +0700

    pertemuan 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+// import 'column_widget.dart';
+// import 'row_widget.dart';
+import 'first_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,23 +13,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Praktikum Mobile Lanjut',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.deepPurple,
         ),
+        useMaterial3: true,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('PRAKTIKUM MOBILE LANJUT'),
-        ),
-        body: Center(
-          child: Text(
-            'Hallo Nama Saya Muhammad Ilham Bintang',
-            style: TextStyle(fontSize: 24),
-          ),
-        ),
-      ),
+      home: FirstWidget(),
     );
   }
 }
